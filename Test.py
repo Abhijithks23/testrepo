ee8ec8f0bc390c19ec3bf95d037315417ecc37c6
Abhijithks23 <119779255+Abhijithks23@users.noreply.github.com>
2023-05-08 17:25:06 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My First Program
+
+# In[1]:
+
+
+1+1
+
+
+# **Problem 1: Find the minimum and maximum values.**
+
+# In[1]:
+
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+
+# **Problem 2: Find the value of 4 to the power 3.**
+
+# In[3]:
+
+
+x = pow(4, 3)
+print(x)
+
+
+# In[ ]:
+
+
+
+
